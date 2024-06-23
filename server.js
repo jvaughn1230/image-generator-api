7703a8963448d4287a1fe3401869ce85e65cf620
Alex <jvaughn1230@gmail.com>
2024-06-23 11:28:18 -0700

    connected to openai API image generator

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
 
+const corsOptions = require("./config/corsOptions");
+const openAIRoute = require("./routes/openaiRoute");
+
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/openai", openAIRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${process.env.PORT || 3500}`);
